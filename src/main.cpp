8458718895809a6d41766510b7fc279aac1e1e36
Xander <xanderperry@hotmail.nl>
2024-03-01 11:15:32 +0100

    Add calibration for accelerometer readings

--- src/main.cpp
+++ src/main.cpp
@@ -21,12 +21,27 @@ void Calculateservos(int x, int y, int z);
 
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
+float correctX, correctY, correctZ;
+
 void setup(void) 
 {
   Serial.begin(9600);
   
   accel.begin();
   accel.setRange(ADXL345_RANGE_4_G);
+
+  float x = 0.0, y = 0.0, z = 0.0;
+  for(int i = 0; i < 100; i++)
+  {
+    sensors_event_t event; 
+    accel.getEvent(&event);
+    x += event.acceleration.x;
+    y += event.acceleration.y;
+    z += event.acceleration.z;
+  }
+  correctX = x / 100.0;
+  correctY = y / 100.0;
+  correctZ = z / 100.0;
 }
 
 void loop(void) 
@@ -34,8 +49,8 @@ void loop(void)
   sensors_event_t event; 
   accel.getEvent(&event);
  
-  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
-  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
-  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
+  Serial.print("X: "); Serial.print(event.acceleration.x - correctX); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.acceleration.y - correctY); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.acceleration.z - correctZ); Serial.print("  ");Serial.println("m/s^2 ");
   delay(500);
 }

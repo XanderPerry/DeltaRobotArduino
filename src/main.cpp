614d602baec1d77ffc1801d5aee6e388345692a8
hayan <hayan.rafa3@gmail.com>
2024-03-01 10:04:36 +0100

    accs test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
-#include <adafruit_ADXL345_U,h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+
 
 /*
 struct Coordinates
@@ -16,13 +19,31 @@ void Calculateservos(int x, int y, int z);
 #define accely A1
 #define accelz A2
 
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified();
 
-void setup() 
-{
-  
+void setup(void) {
+  Serial.begin(9600);
+
+  if (!accel.begin()) {
+    Serial.println("No ADXL345 sensor detected.");
+    while (1)
+      ;
+  }
 }
 
-void loop() 
-{
-
+void loop(void) {
+  Serial.println("test");
+  sensors_event_t event;
+  accel.getEvent(&event);
+  Serial.print("X: ");
+  Serial.print(event.acceleration.x);
+  Serial.print("  ");
+  Serial.print("Y: ");
+  Serial.print(event.acceleration.y);
+  Serial.print("  ");
+  Serial.print("Z: ");
+  Serial.print(event.acceleration.z);
+  Serial.print("  ");
+  Serial.println("m/s^2 ");
+  delay(500);
 }

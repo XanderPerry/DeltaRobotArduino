34d0ecc0011a38f7d04b6fa8e6ec297755f38ba7
Xander <xanderperry@hotmail.nl>
2024-03-14 11:31:38 +0100

    Add calculation functions for arm angles

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,19 @@ struct Coordinates
   int z;
 };
 
+struct Angles
+{
+  byte hoek1;
+  byte hoek2;
+  byte hoek3;
+};
+
 float DegreeToRadian(int degree);
 Coordinates ask_coordinates(void);
 char calc_arm1(Coordinates coords);
+char calc_arm2(Coordinates coords);
+char calc_arm3(Coordinates coords);
+Angles calc_angles(Coordinates coords);
 
 void setup()
 {
@@ -39,6 +49,17 @@ void loop()
   Serial.println(coord.y);
   Serial.print("z: ");
   Serial.println(coord.z);
+
+  Angles angles = calc_angles(coord);
+  Serial.print("hoek1: ");
+  Serial.println(angles.hoek1);
+  Serial.print("hoek2: ");
+  Serial.println(angles.hoek2);
+  Serial.print("hoek3: ");
+  Serial.println(angles.hoek3);
+  Serial.println("");
+
+  delay(2000);
 }
 
 // geeft van een int in graden een float in radialen met 2 decimalen
@@ -81,13 +102,86 @@ char calc_arm1(Coordinates coords)
   for(int i = starthoek; i < eindhoek; i++)
   {
     int z_elleboog = z_servo + Larm1 * sin(DegreeToRadian(i));
+    int x_elleboog = x_servo + Larm1 * cos(DegreeToRadian(i));
+    int afstand = sqrt(pow(x - x_elleboog, 2) + pow(y - y_servo, 2) + pow(z - z_elleboog, 2));
 
-    if((afstand - marge < Larm2) || (afstand + marge > Larm2))
+    if((afstand - marge < Larm2) && (afstand + marge > Larm2))
     {
+      // Serial.print("Oplossing gevonden, hoek 1 is: ");
+      // Serial.print(i);
+      // Serial.println(" graden!");
       return i;
     }
   }
+  Serial.println("Geen oplossing gevonden");
+  return 0;  
+}
+
+char calc_arm2(Coordinates coords)
+{
+  int x = coords.x - cos(DegreeToRadian(60)) * Ltop;
+  int y = coords.y + sin(DegreeToRadian(60)) * Ltop;
+  int z = coords.z;
   
+  int x_servo = -cos(DegreeToRadian(60)) * Larmbase;
+  int y_servo = sin(DegreeToRadian(60)) * Larmbase;
+  int z_servo = 0;
+
+  for(int i = starthoek; i < eindhoek; i++)
+  {
+    int x_elleboog = x_servo - cos(DegreeToRadian(60)) * Larm1 * cos(DegreeToRadian(i));
+    int y_elleboog = y_servo + sin (DegreeToRadian(60)) * Larm1 * sin(DegreeToRadian(i));
+    int z_elleboog = z_servo + Larm1 * sin(DegreeToRadian(i));
+    int afstand = sqrt(pow(x - x_elleboog, 2) + pow(y - y_elleboog, 2) + pow(z - z_elleboog, 2));
+
+    if((afstand - marge < Larm2) && (afstand + marge > Larm2))
+    {
+      // Serial.print("Oplossing gevonden, hoek 2 is: ");
+      // Serial.print(i);
+      // Serial.println(" graden!");
+      return i;
+    }
+  }
+  Serial.println("Geen oplossing gevonden");
+  return 0;
+}
+
+char calc_arm3(Coordinates coords)
+{
+  int x = coords.x - cos(DegreeToRadian(60)) * Ltop;
+  int y = coords.y - sin(DegreeToRadian(60)) * Ltop;
+  int z = coords.z;
+  
+  int x_servo = -cos(DegreeToRadian(60)) * Larmbase;
+  int y_servo = -sin(DegreeToRadian(60)) * Larmbase;
+  int z_servo = 0;
+
+  for(int i = starthoek; i < eindhoek; i++)
+  {
+    int x_elleboog = x_servo - cos(DegreeToRadian(60)) * Larm1 * cos(DegreeToRadian(i));
+    int y_elleboog = y_servo - sin (DegreeToRadian(60)) * Larm1 * sin(DegreeToRadian(i));
+    int z_elleboog = z_servo + Larm1 * sin(DegreeToRadian(i));
+    int afstand = sqrt(pow(x - x_elleboog, 2) + pow(y - y_elleboog, 2) + pow(z - z_elleboog, 2));
+
+    if((afstand - marge < Larm2) && (afstand + marge > Larm2))
+    {
+      // Serial.print("Oplossing gevonden, hoek 3 is: ");
+      // Serial.print(i);
+      // Serial.println(" graden!");
+      return i;
+    }
+  }
+  Serial.println("Geen oplossing gevonden");
+  return 0;
+}
+
+Angles calc_angles(Coordinates coords)
+{
+  Angles angles;
+  angles.hoek1 = calc_arm1(coords);
+  angles.hoek2 = calc_arm2(coords);
+  angles.hoek3 = calc_arm3(coords);
+  return angles;
 }
 
 #endif

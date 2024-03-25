88622357097559e30b2ebd869a6e65d47af1bfe1
Nick Piek <Nickpiek2004@gmail.com>
2024-03-25 12:42:13 +0100

    test

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -1,3 +1,19 @@
 #ifdef TEST
+#include <Arduino.h>
+#include <Stepper.h>
+int motorspeed = 10;
+Stepper myStepper(2048, 8, 9, 10, 11);
 
+void setup()
+{
+    myStepper.setSpeed(motorspeed);
+}
+
+void loop()
+{
+    myStepper.step(2048);
+    delay(1000);
+    myStepper.step(-2048);
+    delay(1000);
+}
 #endif
